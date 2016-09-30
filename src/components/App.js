fc41b7986be23ace87f1c97ce0e5bc02227bf2cd
Eric Douglas <eric.douglas.mail@gmail.com>
2016-09-30 07:37:51 -0300

    Initial structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CommentBox from './CommentBox';
+
+class App extends Component {
+  render() {
+    return (
+      <CommentBox />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/CommentBox/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class CommentBox extends Component {
+  render() {
+    return (
+      <div className="comment-box">
+        Hello, React! I am a CommentBox.
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
