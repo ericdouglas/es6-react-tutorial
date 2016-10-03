33b4943908914d845c0b3b2fff5c280bd9dc562b
Eric Douglas <eric.douglas.mail@gmail.com>
2016-10-03 07:39:57 -0300

    Pass data to CommentList

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CommentBox from './components/CommentBox';
 
 const data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 1, author: "Pete Hunt", text: "This is **one comment**"},
   {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
 ];
 
