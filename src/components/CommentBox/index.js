6b6545203d069a07953b60da4cfe34c905da1430
Eric Douglas <eric.douglas.mail@gmail.com>
2016-10-03 06:48:27 -0300

    Add basic CommentForm and CommentList components

--- src/components/CommentBox/index.js
+++ src/components/CommentBox/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './index.css';
 
+import CommentList from '../CommentList';
+import CommentForm from '../CommentForm';
+
 class CommentBox extends Component {
   render() {
     return (
       <div className="comment-box">
-        Hello, React! I am a CommentBox.
+        <h1>Comments</h1>
+
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
