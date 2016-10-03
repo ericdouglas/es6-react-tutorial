9e3a3d807acbdd43d932580c154185bd6de6291d
Eric Douglas <eric.douglas.mail@gmail.com>
2016-10-03 07:20:26 -0300

    Remove App from components folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import CommentBox from './components/CommentBox';
 
 class App extends Component {
   render() {
     return (
+      <CommentBox />
     );
   }
 }
